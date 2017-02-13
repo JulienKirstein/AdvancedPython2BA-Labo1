7c49ae98b9884b74b28ada9a1404abd8e7353767
Julien Kirstein <j1996kirstein@gmail.com>
2017-02-13 14:44:58 +0100

    julien

--- Fichiers/test_utils.py
+++ Fichiers/test_utils.py
@@ -8,17 +8,36 @@ import utils
 class TestUtils(unittest.TestCase):
     def test_fact(self):
         # À compléter...
+        try:
+            self.assertEqual(utils.fact(5), 120)
+        except:
+            print("mort")
+
         pass
     
     def test_roots(self):
         # À compléter...
+        try:
+            self.assertEqual(utils.roots(1, 0, 1), 0)
+        except:
+            print("mort")
         pass
     
     def test_integrate(self):
         # À compléter...
+        try:
+            self.assertEqual(utils.integrate('x ** 2 - 1', -1, 1), 120)
+        except:
+            print("mort")
         pass
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
     runner = unittest.TextTestRunner()
     exit(not runner.run(suite).wasSuccessful())
+
+
+
+
+
+
